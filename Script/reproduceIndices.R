b82739b6fb75c7428129dfd2183cf2dfde73cd5d
olavbr <olavbr@nr.no>
2018-05-31 13:46:24 +0200

    Can investigate sensitivity of sd(mCPUE) w.r.t. removal of otholits

--- Script/reproduceIndices.R
+++ Script/reproduceIndices.R
@@ -71,9 +71,9 @@ mCPUEBasedStratifiedHLmodelALK = CPUEnorthSea(species = species, year = year, qu
 
 #Remove parts of the data and see what happens-----------------
 removeProcedure = "random"
-propRemove = 0.9 #Proportion to remove
+propRemove = 0.01 #Proportion to remove
 outerBootstrapN = 5
-whatToInvestigate = "mean"
+whatToInvestigate = "mean" #whatToInvestigate = "sd"
 typeOfAreaToInvestigate = "RFA" #typeOfAreaToInvestigate = "wholeNorthSea"
 whatToRemove = "CA"
 
