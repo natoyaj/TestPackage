29db9d66e5e969e9694b575f8dd8465d7b03cc29
olavbr <olavbr@nr.no>
2018-06-13 17:39:28 +0200

    test

--- Script/reproduceIndices.R
+++ Script/reproduceIndices.R
@@ -1,12 +1,12 @@
 #Read data----------
-year = 2013
+year = 2015
 quarter = 1
 dat = readIBTSData(survey = "NS-IBTS", year = year, quarter = quarter)
 #-------------------
 
 
 #Set some additional settings--------
-RFA = 1
+RFA = 2
 species = "Gadus morhua"; #species = "Pollachius virens"
 n=3 #Number of bootstrap samples
 #------------------------------------
