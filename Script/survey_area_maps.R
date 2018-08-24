1169aa6315d3c216d7881394211cb2b5e51cfa0e
edvinf <edvin.fuglebakk@gmail.com>
2018-08-24 17:30:55 +0200

    Change plotting to fixed size device (pdf) and tweaked some parameters. Reversed sign o bathymetry, in order to annotate as depth.

--- Script/survey_area_maps.R
+++ Script/survey_area_maps.R
@@ -13,45 +13,47 @@ rfa <-
     "Roundfish_shapefiles"
   ))
 rfa_labelpos <- coordinates(rfa)
+rfa_labelpos[9,1] <- rfa_labelpos[9,1]+0.4 #adjust position for area 3
 rfa_labelpos[6,1] <- rfa_labelpos[6,1]+0.2 #adjust position for area 5
 rfa_labelpos[6,2] <- rfa_labelpos[6,2]+0.3 #adjust position for area 5
 rfa_labelpos[6,2] <- rfa_labelpos[6,2]+0.2 #adjust position for area 5
+rfa_labelpos[5,2] <- rfa_labelpos[5,2]+0.2 #adjust position for area 6
 rfa_labelpos[9,1] <- rfa_labelpos[9,1]+0.3 #adjust position for area 3
-rfa_labelpos[2,1] <- rfa_labelpos[2,1]-0.3 #adjust position for area 9
-rfa_labelpos[2,2] <- rfa_labelpos[2,2]+0.2 #adjust position for area 9
+rfa_labelpos[2,1] <- rfa_labelpos[2,1] #adjust position for area 9
+rfa_labelpos[2,2] <- rfa_labelpos[2,2]+0.1 #adjust position for area 9
 
 ices_rectangles <- readOGR(file.path(system.file("shapefiles", package = "TestPackage"), "ICES_StatRec_mapto_ICES_Areas"), "StatRec_map_Areas_Full_20170124")
 ices_midpoints <- SpatialPoints(as.data.frame(coordinates(ices_rectangles)), proj4string=CRS(proj4string(ices_rectangles)))
 ibts_rectangles <- ices_rectangles[!is.na(unlist(sp::over(ices_midpoints, rfa))),]
 
-ns <- readRDS(file.path(system.file("bathymetry", package = "TestPackage"), "wgs84_raster_north_sea_crop", "ns_halfres.rds"))
+ns <- readRDS(file.path(system.file("bathymetry", package = "TestPackage"), "wgs84_raster_north_sea_crop", "ns_halfres.rds"))*-1
 data(countriesHigh)
 map <- countriesHigh
 
 #Needs fiddling when final output dimensions are decided
-map_labels <- c("Denmark", "France", "Germany", "Netherlands", "Norway", "UK Scotland", "Sweden", "UK England")
+map_labels <- c("Denm.", "France", "Germany", "Netherl.", "Norway", "UK Scotl.", "Sweden", "UK Engl.")
 map_labelpoints <- t(matrix(c(
-  10.5, 55.7,
+  10.9, 55.6,
   2.5, 49.3,
   10, 51,
   6.9, 52.28,
-  9.4, 60.5,
-  -4.8, 57,
+  9.4, 60.7,
+  -5, 57,
   14, 59,
   -1.9, 52.4), ncol=length(map_labels), nrow=2))
 
 #
 # colors
 #
-batpal <- rev(brewer.pal(9, "Blues"))
-batpal <- c(rep(batpal[1],4),rep(batpal[2],4), rep(batpal[3],4), batpal[4:length(batpal)])
+batpal <- brewer.pal(9, "Blues")
+batpal <- c(batpal[1:(length(batpal)-3)], rep(batpal[7],4),rep(batpal[8],4), rep(batpal[9],4))
 
 
-plot_ibts_map <- function(polygons = rfa, labelcol="AreaName", labelpos=rfa_labelpos, polygonscol="black", rectangles=ibts_rectangles, rectanglecol="lightgray", landmap=map, landcol="lightgrey", landbordercol="darkgrey", landlabels=map_labels, land_labelpoints=map_labelpoints, land_labelcol="black", cex.landlabels=0.8, seacol="white", bat=ns, batcol=batpal, xlim=c(-8,18), ylim=c(49,62.5), xlab="Longitude", ylab="Latitude"){
+plot_ibts_map <- function(polygons = rfa, labelcol="AreaName", labelpos=rfa_labelpos, polygonscol="black", cex.polygons=0.8, rectangles=ibts_rectangles, rectanglecol="lightgray", landmap=map, landcol="lightgrey", landbordercol="darkgrey", landlabels=map_labels, land_labelpoints=map_labelpoints, land_labelcol="black", cex.landlabels=0.6, seacol="white", bat=ns, batcol=batpal, xlim=c(-8,18), ylim=c(49,62.5), xlab="Longitude", ylab="Latitude"){
 
   if (!is.null(bat)){
     ext <- as(extent(xlim[1], xlim[2], ylim[1], ylim[2]), 'SpatialPolygons')
-    plot(crop(bat, ext), col=batcol, xlab=xlab, ylab=ylab)
+    plot(crop(bat, ext), col=batcol, xlab=xlab, ylab=ylab, legend.args=list(text="depth (m)"))
   }
   else{
     basemap(
@@ -71,7 +73,7 @@ plot_ibts_map <- function(polygons = rfa, labelcol="AreaName", labelpos=rfa_labe
       if (is.null(labelpos)){
         labelpos <- coordinates(polygons)
       }
-      text(labelpos, as.character(polygons@data[[labelcol]]), col=polygonscol)
+      text(labelpos, as.character(polygons@data[[labelcol]]), col=polygonscol, cex=cex.polygons)
     }
     if (!is.null(land_labelpoints)){
       text(land_labelpoints, landlabels, cex=cex.landlabels, col=land_labelcol)
@@ -79,8 +81,14 @@ plot_ibts_map <- function(polygons = rfa, labelcol="AreaName", labelpos=rfa_labe
   }
 }
 
-old.par <- par(no.readonly = T)
-par(mfrow=c(1,2), mar=c(5.1, 4.1, 4.1, 4.1))
-plot_ibts_map(bat = NULL, ylab="", cex.landlabels=0.6)
-plot_ibts_map(rectangles = NULL, cex.landlabels=0.6)
-par(old.par)
+plot_on_pdf <- function(filename){
+  pdf(filename, width=8, height=4)
+  old.par <- par(no.readonly = T)
+  par(mfrow=c(1,2), mar=c(5.1, 4.1, 4.1, 4.1))
+  plot_ibts_map(bat = NULL, land_labelpoints = NULL)
+  par(mar=c(5.1, 1.1, 4.1, 7.1))
+  plot_ibts_map(rectangles = NULL, ylab="", labelcol = NULL)
+  par(old.par)
+  dev.off()
+}
+plot_on_pdf("surveyarea.pdf")
