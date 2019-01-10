21cf15c6c5b1d0818e8ae7d3fcf7e2ae38feb6c3
olavbr <olavbr@nr.no>
2019-01-10 12:51:33 +0100

    More cleaning of code

--- R/simDatrasData.R
+++ R/simDatrasData.R
@@ -198,152 +198,62 @@ simCaHlSimultaniousyStratified = function(RFA,year, quarter,dataHH, loc = NULL)
 
 }
 
-#'
-#' resampling for hiearchical bootstrap. Strucutre is a bit different from the above to ensure that haul selection is consistent between the different tables.
-#' simTrawlHaulsHiearchical could probably be modified to make parameters quite similar to the ones above.
-#'
-
-#' selectHaulIdsHiearchical
-#' @description Makes hiearchical selection of haul ids for bootstrap estimation.
-#' @param hh Data set of hauls, formatted as the DATRAS table (could be HH, CA or HL), but containing at most one row pr. haul.
-#' @param levels vector with names of columns in hh that represents levels in the hiearchical sampling. levels[1] identifies the primary sampling unit, levels[2] the secondary, and so on.
-#' @param selection codes for how selection is done: "R" selection with replacement (sample size: the number of available samples), "S" selection without replacement with sample size a selected as a random integer between 1 and the number of available samples (inclusive), "N": No selection or selection without replacement with sample size: the number of available samples
-#' @param idname the name of a column that identifies samples
-#' @param seed seed for selection, if NULL seed will not be set.
-#' @return a vector of haul ids, possibly repeating. Note: Does not return a data structure suitable for feeding into functions for CPUE-estimates
-#' @keywords internal
-selectHaulIdsHiearchical <-
-  function(hh,
-           levels,
-           selection,
-           seed = NULL) {
-    if (length(levels) != length(selection)) {
-      stop("selection codes must match the number of levels in hiearchy")
-    }
-    if (length(levels) < 1) {
-      stop("Must provide at least one level")
-    }
-
-    idname = "haul.id"
-    if (any(duplicated(hh[,c(idname, levels)]))){
-      stop(paste("levels and ", idname, "does not uniquely identify rows."))
-    }
-
-
-    #
-    # make selection at level
-    #
-    levelcolumn <- levels[1]
-    selectionmethod <- selection[1]
-    levelvalues <- unique(hh[, levelcolumn])
-    if (!is.null(seed)) {
-      set.seed(seed)
-    }
-    if (selectionmethod == "R") {
-      levelvalues <- sample(levelvalues, size=length(levelvalues), replace=T)
-    }
-    else if (selectionmethod == "S") {
-      n <- sample(1:length(levelvalues), size=1, replace=F)
-      levelvalues <- sample(levelvalues, size=n, replace=F)
-    }
-    else if (selectionmethod == "N") {
-      levelvalues <- sample(levelvalues, size=length(levelvalues), replace=F)
-    }
-    else{
-      stop(paste("Invalid selection method", selectionmethod))
-    }
-
-    #
-    # make selection on lower levels
-    #
-    ids <- c()
-
-    #base case, no lower levels exists
-    if (length(levels) == 1) {
-      for (v in levelvalues) {
-        ids <- c(ids, as.vector(hh[hh[, levelcolumn] == v, idname]))
-      }
-    }
-
-    #recursive case, lower levels exists
-    else if (length(levels) > 1) {
-      if (selectionmethod=="R"){
-        stop("selection with replacement is not supported at higher levels. Need to implement renaming of sampling units.")
-      }
-      sublevels <- levels[2:length(levels)]
-      subselection <- selection[2:length(selection)]
-      for (v in levelvalues) {
-        ids <-
-          c(ids,
-            selectHaulIdsHiearchical(hh[hh[, levelcolumn] == v,], sublevels, subselection, NULL))
-      }
-    }
-    else{
-      stop("ERROR: Recursed past base case")
-    }
-
-    return(ids)
-  }
-
-#' simTrawlHaulsBySelection
-#' @description selects a possibly repeating sample with generated unique ids for bootstrap
-#' @param data data frame to resample, e.g. CA or HL table from DATRAS
-#' @param idvalues values of the column haul.id that defines the selection
-#' @param oldidname column name where original haul ids should be stored
-#' @param removeMIssing indicates whether idvalues with no corresponding entries in data should be rmeoved. If FALSE, this case halts execution
-#' @return data frame with the specified selection of samples, with column haul.id populated with unqiue identifiers in stead of the original identifiers, and the original identifiers stored in the column oldidname. idvalues not in data will be represented by NAs
-#' @keywords internal
-simTrawlHaulsBySelection <- function(data, idvalues, oldidname="original.id", removeMissing=F){
-  idname="haul.id"
-
-  hauls <- data.frame(oldid=idvalues, newid=as.factor(1:length(idvalues)))
-  hauls[,oldidname] <- hauls$oldid
-
-  if (!all(hauls$oldid %in% data[,idname]) & removeMissing){
-    hauls <- hauls[hauls$oldid %in% data[,idname],]
-  }
-  else if (!all(hauls$oldid %in% data[,idname]) & !removeMissing){
-    stop("Not all idvalues found in data")
-  }
-
-  simdata <- merge(data, hauls, by.x=idname, by.y="oldid")
-  simdata[,idname] <- simdata$newid
-  simdata$newid <- NULL
-
-  return(simdata)
-}
-
-#' simTrawlHaulsHiearchical
-#' @description Simulates selection of hauls by selection of StatRec without replacement, and selection of haul.id with replacement
-#' @param RFA Roundfish area number.
-#' @param year The year of interest.
-#' @param quarter The quarter of interest.
-#' @param hl data formated as DATRAS HL table
-#' @param ca data formated as DATRAS CA table
-#' @returns list of tables with formatted the same way, but with artificial haul.ids, original haul.id is preserved in column original.id
+#' sampleCA
+#' @description .
+#' @param datDetailed The quarter of interest.
 #' @export
-simTrawlHaulsHiearchical <- function(RFA, year, quarter, hl, ca, hierarchy=c("StatRec", "haul.id"), selection=c("S","R")){
-
-  dataOfInterest <- function(data){
-    return(data[!is.na(data$Year) & data$Year == year&
-           !is.na(data$Quarter) & data$Quarter == quarter&
-           !is.na(data$Roundfish) & data$Roundfish == RFA ,])
+#' @return Returns a modified data set of the data used for calculating the CPUE. The data is modified by removing
+#' observations in a certain procedure.
+#' @examples
+sampleCA = function(datDetailed,species,quarter,lengthDivision,samplesWithinEachIntervall,hl_hh){
+  toReturn = datDetailed
+  toReturn = datDetailed[1,]
+  for(id in unique(datDetailed$haul.id)){
+    obsTmp = datDetailed[which(datDetailed$haul.id==id),]
+    obsReduced = sampleCAHaul(obsTmp,lengthDivision, samplesWithinEachIntervall,species = species,hl_hh = hl_hh)
+    toReturn = rbind(toReturn,obsReduced)
   }
+  toReturn = toReturn[-1,]
 
-  hl <- dataOfInterest(hl)
-  ca <- dataOfInterest(ca)
-
-  if (any(!(ca$haul.id %in% hl$haul.id))){
-    stop("Some age measurements does not have corresponding length measurements")
-  }
-
-  hlframe <- hl[!duplicated(hl[,c(hierarchy, "haul.id")]),]
-
-  haul.ids <- selectHaulIdsHiearchical(hlframe, hierarchy, selection)
-
-  ret <- list()
-  ret$simHL <- simTrawlHaulsBySelection(hl, haul.ids, oldidname="original.id", removeMissing=F)
-  ret$simCA <- simTrawlHaulsBySelection(ca, haul.ids, oldidname="original.id", removeMissing=T)
-
-  return(ret)
+  return(toReturn)
 }
+
+#' sampleCAHaul
+#' @description .
+#' @param datDetailed The quarter of interest.
+#' @param RFA The roundfish area of interest.
+#' @export
+#' @return Returns a modified data set of the data used for calculating the CPUE. The data is modified by removing
+#' observations in a certain procedure.
+#' @examples
+sampleCAHaul = function(obsTmp,lengthDivision,samplesWithinEachIntervall,species,hl_hh){
+  toReturn = obsTmp[1,]
+  for(i in 2:(length(lengthDivision)+1)){
+    if(i <= length(lengthDivision)){
+      obsInside = which(obsTmp$LngtCm>=lengthDivision[i-1]  & obsTmp$LngtCm<lengthDivision[i])
+    }else if(i>length(lengthDivision)){
+      obsInside = which(obsTmp$LngtCm>=lengthDivision[i-1])
+    }
+    if(length(obsInside)>1){
+      prob = rep(0,length(obsInside))
+      for(j in 1:length(obsInside)){
+        prob[j] = obsInHL(species = species,hl_hh = hl_hh,id = unique(obsTmp$haul.id),length = obsTmp$LngtCm[obsInside[j]])
+      }
+      nSample = min(samplesWithinEachIntervall,length(obsInside))
+      obsSelected=NULL
+      for(i in 1:nSample){
+        obsSelectedCurrent = sample(obsInside,1,prob = prob)#Sample with probability proportional to the number observed at length
+        prob[which(obsTmp$LngtCm[obsInside]== obsTmp$LngtCm[obsSelectedCurrent])] = prob[which(obsTmp$LngtCm[obsInside]== obsTmp$LngtCm[obsSelectedCurrent])] -1 #Update the probability for sampling the next age observation
+        obsSelected = c(obsSelected,obsSelectedCurrent)
+      }
+    }else{
+      obsSelected = obsInside
+    }
+    toReturn = rbind(toReturn,obsTmp[obsSelected,])
+  }
+  toReturn = toReturn[-1,]
+
+  return(toReturn)
+}
+
+
