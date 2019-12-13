76abdb2d71ed574f8d1793e8c66364bd78bc942c
edvinf <edvin.fuglebakk@gmail.com>
2019-12-13 23:37:17 +0100

    changed repeating x-labels and legend on overlplots

--- script/TablePlotsOverlay.R
+++ script/TablePlotsOverlay.R
@@ -179,7 +179,7 @@ stackedPanelsOverlay <- function(data, columnGroups, rowGroups, xVariable, yVari
 #' Same x-axis, and row-categories, but two different y-axis for the different groups.
 #' values for each of the column groups are plotted on top of each other in the column to the right for yvariable1
 #' values for each of the column groups are plotted on top of each other in the column to the left for yvariable2
-overlayedColumnPlot <- function(data, columnGroups, rowGroups, xVariable, yVariable1, yVariable2, yVariable1Lower=NULL, yVariable1Upper=NULL, yVariable2Lower=NULL, yVariable2Upper=NULL, ylab1=NULL, ylab2=NULL, xlab=NULL, xlim=NULL, ymin1=0, ymin2=0, ymax1=NULL, ymax2=NULL, pointcol="black", linecol="#cb181d", errorcol="#cb181d", tickmarksY1=NULL, tickmarksY2=NULL, basetheme=function(x){ggplot2::theme_classic() + theme(plot.title = element_text(hjust = 0.5), axis.text.y = element_text(angle = 90, hjust = 1, size=6), axis.text.x=element_text(size=6))}){
+overlayedColumnPlot <- function(data, columnGroups, rowGroups, xVariable, yVariable1, yVariable2, yVariable1Lower=NULL, yVariable1Upper=NULL, yVariable2Lower=NULL, yVariable2Upper=NULL, ylab1=NULL, ylab2=NULL, xlab=NULL, xlim=NULL, ymin1=0, ymin2=0, ymax1=NULL, ymax2=NULL, pointcol="black", linecol="#cb181d", errorcol="#cb181d", tickmarksY1=NULL, tickmarksY2=NULL, basetheme=function(x){ggplot2::theme_classic() + theme(plot.title = element_text(hjust = 0.5), axis.text.y = element_text(angle = 90, hjust = 1, size=6), axis.text.x=element_text(size=6), legend.text = element_text(size=8))}){
   if(is.numeric(columnGroups) | is.numeric(rowGroups)){
     stop("ColumnGroups and rowGroups can not be numeric variables. Covert with as.character()")
   }
@@ -291,12 +291,12 @@ overlayedColumnPlot <- function(data, columnGroups, rowGroups, xVariable, yVaria
     
     plotdata <- data[data[,rowGroups] == row,]
     #plot yvar1
-    panel <- panelPlotOverlay(plotdata, columnGroups, xVariable, yVariable1, yVariable1Upper, yVariable1Lower, xlimrow, ylim1col, showX=T, showY=T, ylabel=row, basetheme=basetheme, title=title1, pointcol=pointcol, linecol=linecol, errorcol=errorcol, tickmarks=tickmarksY1)
+    panel <- panelPlotOverlay(plotdata, columnGroups, xVariable, yVariable1, yVariable1Upper, yVariable1Lower, xlimrow, ylim1col, showX=(row == rows[length(row)]), showY=T, ylabel=row, basetheme=basetheme, title=title1, pointcol=pointcol, linecol=linecol, errorcol=errorcol, tickmarks=tickmarksY1)
     panel <- panel + theme(legend.position="none")
     panels[[paste(row, "yvar1", sep="/")]] <- panel
     
     #plot yvar2
-    panel <- panelPlotOverlay(plotdata, columnGroups, xVariable, yVariable2, yVariable2Upper, yVariable2Lower, xlimrow, ylim2col, showX=T, showY=T, ylabel=NULL, basetheme=basetheme, title=title2, pointcol=pointcol, linecol=linecol, errorcol=errorcol, tickmarks=tickmarksY2)
+    panel <- panelPlotOverlay(plotdata, columnGroups, xVariable, yVariable2, yVariable2Upper, yVariable2Lower, xlimrow, ylim2col, showX=(row == rows[length(row)]), showY=T, ylabel=NULL, basetheme=basetheme, title=title2, pointcol=pointcol, linecol=linecol, errorcol=errorcol, tickmarks=tickmarksY2)
     panel <- panel + theme(legend.position="none")
     panels[[paste(row, "yvar2", sep="/")]] <- panel
     
@@ -333,15 +333,17 @@ alkresult$bs <- NULL
 alkresult[alkresult$Bootstrap=="Stratified", "bs"] <- "S"
 alkresult[alkresult$Bootstrap=="ICES-IBTS", "bs"] <- "I"
 alkresult[alkresult$Bootstrap=="Modified-ICES", "bs"] <- "mI"
-alkresult$ALKm <- paste(alkresult$ALK, alkresult$bs, sep=", ")
+alkresult[alkresult$ALK == "area based", "ALK"] <- "area-based"
+alkresult[alkresult$ALK == "haul based", "ALK"] <- "haul-based"
+alkresult$ALKm <- paste(alkresult$ALK, alkresult$bs, sep=" ")
 alkresult$age <- paste("Age", alkresult$age)
 alkresult$RSE <- alkresult$sd / alkresult$mCPUE
 
 
 estimator_colors <- list()
-estimator_colors[["area based, I"]] <- "#92c5de"
-estimator_colors[["area based, mI"]] <- "#0571b0"
-estimator_colors[["haul based, S"]] <- "#ca0020"
+estimator_colors[["area-based I"]] <- "#92c5de"
+estimator_colors[["area-based mI"]] <- "#0571b0"
+estimator_colors[["haul-based S"]] <- "#ca0020"
 
 ####
 # Plot 1
